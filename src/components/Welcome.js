0f6a844fb520d0a2f282b6690afc4528abf2f1af
Ioan Vulturar <ioan.vulturar@fanduel.com>
2021-07-16 17:24:10 +0300

    cleaning after merging

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Welcome = ({ username }) => {
+export default function Welcome({ username }) {
     return (
 
         <div className="container" >
-            <div className="card mt-5" style={{ width: "500px", margin: "auto" }}>
+            <div className="card mt-5 form-width">
                 <div className="card-body text-center">
                     <h2>Welcome, <span>{username}!</span></h2>
                 </div>
